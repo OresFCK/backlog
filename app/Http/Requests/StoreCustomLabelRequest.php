f2131c8881205782112539caa1aeedbe878fc5b7
OresFCK <you@example.com>
2026-05-22 00:35:11 +0200

    fix

--- app/Http/Requests/StoreCustomLabelRequest.php
+++ app/Http/Requests/StoreCustomLabelRequest.php
@@ -3,29 +3,9 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Validator;
 
 class StoreCustomLabelRequest extends FormRequest
 {
-    private const BLOCKED_WORDS = [
-        'hitler',
-        'nazi',
-        '1488',
-        'heilhitler',
-        'nigger',
-        'nigga',
-        'nigg',
-        'kike',
-        'chink',
-        'spic',
-        'faggot',
-        'tranny',
-        'isis',
-        'alqaeda',
-        'pedo',
-        'pedophile',
-    ];
-
     public function authorize(): bool
     {
         return true;
@@ -48,54 +28,49 @@ class StoreCustomLabelRequest extends FormRequest
         ];
     }
 
-    public function messages(): array
+    public function withValidator($validator): void
     {
-        return [
-            'title.required' => 'Label title is required.',
-            'title.min' => 'Label title must be at least 2 characters.',
-            'title.max' => 'Label title cannot be longer than 24 characters.',
-            'color.required' => 'Label color is required.',
-            'color.regex' => 'Label color must be a valid hex color.',
-        ];
-    }
+        $validator->after(function ($validator) {
 
-    public function withValidator(Validator $validator): void
-    {
-        $validator->after(function (Validator $validator) {
-            if ($this->containsBlockedWord()) {
-                $validator
-                    ->errors()
-                    ->add(
-                        'title',
-                        'This label title is not allowed.'
-                    );
+            $blocked = [
+                'hitler',
+                'nazi',
+                '1488',
+                'heilhitler',
+                'nigger',
+                'nigga',
+                'nigg',
+                'kike',
+                'chink',
+                'spic',
+                'faggot',
+                'tranny',
+                'isis',
+                'alqaeda',
+                'pedo',
+                'pedophile',
+            ];
+
+            $title = str(
+                $this->title
+            )->lower();
+
+            foreach ($blocked as $word) {
+
+                if (
+                    $title->contains($word)
+                ) {
+
+                    $validator
+                        ->errors()
+                        ->add(
+                            'title',
+                            'Blocked word detected.'
+                        );
+
+                    break;
+                }
             }
         });
-    }
-
-    private function containsBlockedWord(): bool
-    {
-        $title = $this->normalizedTitle();
-
-        foreach (self::BLOCKED_WORDS as $word) {
-            if (str_contains($title, $word)) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-
-    private function normalizedTitle(): string
-    {
-        $title = strtolower(
-            $this->input('title', '')
-        );
-
-        return preg_replace(
-            '/[^a-z0-9]/',
-            '',
-            $title
-        );
     }
 }
