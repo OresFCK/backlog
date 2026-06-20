f50383716b766ffff053edd652c29fe732b5ef52
OresFCK <you@example.com>
2026-06-20 23:41:21 +0200

    debuging

--- resources/js/components/layout/Sidebar.vue
+++ resources/js/components/layout/Sidebar.vue
@@ -90,18 +90,36 @@ const stopLoader = () => {
 }
 
 onMounted(() => {
-    removeStart = router.on('start', () => {
+    removeStart = router.on('start', event => {
+        console.log('INERTIA start', event)
+
         stopLoader()
 
         timeout = setTimeout(() => {
+            console.log('LOADER true')
             pageLoading.value = true
         }, 500)
     })
 
-    removeFinish = router.on('finish', stopLoader)
-    removeError = router.on('error', stopLoader)
-    removeInvalid = router.on('invalid', stopLoader)
-    removeException = router.on('exception', stopLoader)
+    removeFinish = router.on('finish', event => {
+        console.log('INERTIA finish', event)
+        stopLoader()
+    })
+
+    removeError = router.on('error', event => {
+        console.log('INERTIA error', event)
+        stopLoader()
+    })
+
+    removeInvalid = router.on('invalid', event => {
+        console.log('INERTIA invalid', event)
+        stopLoader()
+    })
+
+    removeException = router.on('exception', event => {
+        console.log('INERTIA exception', event)
+        stopLoader()
+    })
 })
 
 onUnmounted(() => {
